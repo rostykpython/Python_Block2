db119e5d3ad8ec7108a55a693019cc94345ecca3
rostykpython <rostyslav.lytvynets@kneu.ua>
2021-10-12 14:46:20 +0300

    HomeTask5_updated

--- module5/hw/hw5.py
+++ module5/hw/hw5.py
@@ -1,18 +1,24 @@
-import time
+import multiprocessing
 
 
-def factorize(*number):
-    result_numbers = [[div_num for div_num in range(1, i + 1) if i % div_num == 0] for i in number]
-    return result_numbers[0] if len(number) == 1 else result_numbers
+def factorize(*numbers):
+    result = []
+    for number in numbers:
+        local_res = []
+        for div_num in range(1, number + 1):
+            if number % div_num == 0:
+                local_res.append(div_num)
+        result.append(local_res)
+    return result[0] if len(numbers) == 1 else result
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    a, b, c, d = factorize(128, 255, 99999, 10651060)
+    with multiprocessing.Pool(3) as pool:
+        a, b, c, d = pool.map(factorize, (128, 255, 99999, 10651060))
 
-    print(f'Time of execution: {time.time() - start_time}')
-    print(a, b, c, d)
+    print(a, b, c, d, sep='\n')
     assert a == [1, 2, 4, 8, 16, 32, 64, 128]
     assert b == [1, 3, 5, 15, 17, 51, 85, 255]
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106,
+                 1521580, 2130212, 2662765, 5325530, 10651060]
